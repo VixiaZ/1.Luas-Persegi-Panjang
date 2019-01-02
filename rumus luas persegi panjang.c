dcabda52168cabfa3e2e698ed05062ce4ee15613
Zulfikar Hidayatullah <45659177+VixiaZ@users.noreply.github.com>
2019-01-03 01:38:26 +0700

    Add files via upload

---
+++ rumus luas persegi panjang.c
@@ -0,0 +1,16 @@
+#include<conio.h>
+int main()
+{
+    int panjang, lebar, luas;
+
+    cout <<"Input Panjang : ";
+    cin >> panjang;
+
+    cout <<"Input Lebar : ";
+    cin >> lebar;
+
+    //Rumus menghitung persegi panjang
+    luas = panjang*lebar;
+    cout << "Luas persegi Panjang :" >> luas ;
+    return 0;
+}
